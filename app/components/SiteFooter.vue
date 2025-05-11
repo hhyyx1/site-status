54e56ab77d064f0f1a2ad8adb05c614cf845e138
古明地こいし <zimamaxiaoan@outlook.com>
2025-05-11 22:28:55 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -32,7 +32,7 @@
       </n-p>
       <n-p depth="3">
         Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        <n-text depth="3" @click="jumpLink(linkData.home)"> 古明地こいし </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/hhyyx1",
+  home: "https://lucky.nlvps.eu.org",
+  email: "mailto:admin@7374520.xyz",
 };
 </script>
 
